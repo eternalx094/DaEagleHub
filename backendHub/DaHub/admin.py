1551e5a7f1756ea18b351be679f03806dcbb7029
eternalx094 <eternalx094@gmail.com>
2025-08-19 16:54:59 +0300

    in process of fixing the shop

--- backendHub/DaHub/admin.py
+++ backendHub/DaHub/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-###################################################################
-from .models import Application
+from .models import Application, CustomUser
 ###################################################################
 @admin.register(Application)
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = ('name', 'logo_img',)
     search_fields = ('name',)
 ###################################################################
+@admin.register(CustomUser)  # This decorator tells Django to use this admin class
+class CustomUserAdmin(admin.ModelAdmin):  # This inheritance is crucial!
+    list_display = ('username', )  # What fields to show in the list view
+    search_fields = ('username', 'email')  # What fields can be searched
--- backendHub/Sledgepong/views.py
+++ backendHub/Sledgepong/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Texture
-#################################################################################################################
+from .models import Player
+############################################################################################################### <-- this is a divider
 def playpage_view(request):
     return render(request, 'Sledgepong/playpage.html')
 ############################################################################################################### <-- this is a divider
 def menu_view(request):
-        return render(request, 'Sledgepong/menu.html')
+    user = request.user
+    sldgpng_player = Player.objects.filter(user=user).first()
+    if not sldgpng_player:
+        Player.objects.create(user=user)
+    return render(request, 'Sledgepong/menu.html')
 ############################################################################################################### <-- this is a divider
 def singles_view(request):
     return render(request, 'Sledgepong/singles.html')
