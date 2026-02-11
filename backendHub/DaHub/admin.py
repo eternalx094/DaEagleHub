94a8fe78bc1b98bb7646a4f97de3c2609dea8bae
eternalx094 <eternalx094@gmail.com>
2026-02-11 15:22:49 +0200

    fixed modeladmin

--- backendHub/DaHub/admin.py
+++ backendHub/DaHub/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Application, CustomUser
-###################################################################
+
+
 @admin.register(Application)
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = ('name', 'logo_img',)
     search_fields = ('name',)
-###################################################################
-@admin.register(CustomUser)  # This decorator tells Django to use this admin class
-class CustomUserAdmin(admin.ModelAdmin):  # This inheritance is crucial!
-    list_display = ('username', )  # What fields to show in the list view
-    search_fields = ('username', 'email')  # What fields can be searched
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):  # 👈 CHANGE THIS
+    model = CustomUser
+    list_display = ('username', 'email', 'coins', 'is_staff')
